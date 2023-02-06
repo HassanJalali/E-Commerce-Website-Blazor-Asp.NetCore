9c861cc3959872a71372d65558c5722dd0dcef01
Hassan Jalali <mister.developer@yahoo.com>
2023-02-06 23:15:50 +0330

    Add Product Controller and calling the first Product Api from the client

--- BlazorEcommerce/BlazorEcommerce/Client/Shared/ProductList.razor.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Shared/ProductList.razor.cs
@@ -1,9 +1,23 @@
 ﻿using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Net.Http.Json;
+
 
 namespace BlazorEcommerce.Client.Shared
 {
     public partial class ProductList
     {
         private List<Product> Products = new();
+
+        protected override async Task OnInitializedAsync()
+        {
+            await GetProduct();
+        }
+        private async Task GetProduct()
+        {
+            Products = await http.GetFromJsonAsync<List<Product>>("api/Product");
+        }
+
     }
 }
---
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BlazorEcommerce.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController : ControllerBase
+    {
+        private static List<Product> Products = new List<Product>
+        {
+            new Product
+            {
+             Id = 1,
+             Title ="شکلات فندقی شونیز 100",
+             Description ="در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
+             Price = 192000,
+             ImageUrl ="https://shoniz.com/wp-content/uploads/2022/08/%D8%B4%D9%88%D9%86%DB%8C%D8%B2-100-%D9%81%D9%86%D8%AF%D9%82%DB%8C.jpg"
+            },
+            new Product
+            {
+             Id=2,
+             Title ="شونيز طلایی شیری فله",
+             Description ="در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
+             Price = 340000,
+             ImageUrl ="https://shoniz.com/wp-content/uploads/2022/08/Shoniz-Gold_Box-15pc-1400.jpg"
+            }
+        };
+
+        [HttpGet]
+        public async Task<IActionResult> GetProduct()
+        {
+            return Ok(Products);
+        }
+    }
+}
+
