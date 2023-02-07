8c19bf499ce20efb033ffb8bc1e2386e3c8ce5bf
Hassan Jalali <mister.developer@yahoo.com>
2023-02-07 21:46:25 +0330

    get product from the database

--- BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Controllers/ProductController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BlazorEcommerce.Server.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace BlazorEcommerce.Server.Controllers
@@ -7,30 +8,17 @@ namespace BlazorEcommerce.Server.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private static List<Product> Products = new List<Product>
-        {
-            new Product
-            {
-             Id = 1,
-             Title ="شکلات فندقی شونیز 100",
-             Description ="در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
-             Price = 192000,
-             ImageUrl ="https://shoniz.com/wp-content/uploads/2022/08/%D8%B4%D9%88%D9%86%DB%8C%D8%B2-100-%D9%81%D9%86%D8%AF%D9%82%DB%8C.jpg"
-            },
-            new Product
-            {
-             Id=2,
-             Title ="شونيز طلایی شیری فله",
-             Description ="در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
-             Price = 340000,
-             ImageUrl ="https://shoniz.com/wp-content/uploads/2022/08/Shoniz-Gold_Box-15pc-1400.jpg"
-            }
-        };
+        private readonly DataContext context;
 
+        public ProductController(DataContext context)
+        {
+            this.context = context;
+        }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProduct()
         {
-            return Ok(Products);
+            var res = await context.Products.ToListAsync();
+            return Ok(res);
         }
     }
 }
