6834e29a48e61c3cb05935bbccbb3ad0f1fb31d8
Hassan Jalali <mister.developer@yahoo.com>
2023-02-07 21:38:28 +0330

    seed data to product table in the database

--- BlazorEcommerce/BlazorEcommerce/Server/Data/DataContext.cs
+++ BlazorEcommerce/BlazorEcommerce/Server/Data/DataContext.cs
@@ -2,13 +2,35 @@
 
 namespace BlazorEcommerce.Server.Data
 {
-    public class DataContext:DbContext
+    public class DataContext : DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options):base(options)
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
 
         }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>().HasData(
+                new Product
+                {
+                    Id = 1,
+                    Title = "شکلات فندقی شونیز 100",
+                    Description = "در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
+                    Price = 192000,
+                    ImageUrl = "https://shoniz.com/wp-content/uploads/2022/08/%D8%B4%D9%88%D9%86%DB%8C%D8%B2-100-%D9%81%D9%86%D8%AF%D9%82%DB%8C.jpg"
+                },
+
+                new Product
+                {
+                    Id = 2,
+                    Title = "شونيز طلایی شیری فله",
+                    Description = "در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.",
+                    Price = 340000,
+                    ImageUrl = "https://shoniz.com/wp-content/uploads/2022/08/Shoniz-Gold_Box-15pc-1400.jpg"
+                });
+        }
     }
 }
---
+++ BlazorEcommerce/BlazorEcommerce/Server/Migrations/20230207180722_productseeding.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace BlazorEcommerce.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class productseeding : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "Description", "ImageUrl", "Price", "Title" },
+                values: new object[,]
+                {
+                    { 1, "در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.", "https://shoniz.com/wp-content/uploads/2022/08/%D8%B4%D9%88%D9%86%DB%8C%D8%B2-100-%D9%81%D9%86%D8%AF%D9%82%DB%8C.jpg", 192000m, "شکلات فندقی شونیز 100" },
+                    { 2, "در جای خشک و خنک، دور از تابش مستقیم نور آفتاب نگهداری شود.", "https://shoniz.com/wp-content/uploads/2022/08/Shoniz-Gold_Box-15pc-1400.jpg", 340000m, "شونيز طلایی شیری فله" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
