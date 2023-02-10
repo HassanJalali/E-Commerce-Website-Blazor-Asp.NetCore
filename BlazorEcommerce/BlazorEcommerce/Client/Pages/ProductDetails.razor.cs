b3e516b92e4535a03a09f51f1919422a369c6f0d
Hassan Jalali <mister.developer@yahoo.com>
2023-02-11 00:23:25 +0330

    show product detail

---
+++ BlazorEcommerce/BlazorEcommerce/Client/Pages/ProductDetails.razor.cs
@@ -0,0 +1,23 @@
+﻿using BlazorEcommerce.Client.Services.ProductService;
+using BlazorEcommerce.Shared;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorEcommerce.Client.Pages
+{
+    public partial class ProductDetails
+    {
+        private Product? product;
+
+        [Parameter]
+        public int Id { get; set; }
+
+        [Inject]
+        private ProductService ProductService { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            product = ProductService.Products.Find(p => p.Id == Id);
+        }
+
+    }
+}
