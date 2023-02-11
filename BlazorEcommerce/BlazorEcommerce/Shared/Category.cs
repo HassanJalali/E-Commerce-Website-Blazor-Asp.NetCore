d686ddc093de3ca9dd95c3960969ac90ebe83bac
Hassan Jalali <mister.developer@yahoo.com>
2023-02-11 21:04:01 +0330

    Add Product Categories model and seed date into it

---
+++ BlazorEcommerce/BlazorEcommerce/Shared/Category.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorEcommerce.Shared
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public String Name { get; set; } = String.Empty;
+        public String Url { get; set; } = String.Empty;
+    }
+}
