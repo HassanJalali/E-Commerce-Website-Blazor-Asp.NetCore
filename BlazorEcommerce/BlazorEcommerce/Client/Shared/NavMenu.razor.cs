66df2eada42edd05bdf6628dc78c6c8098089ce1
Hassan Jalali <mister.developer@yahoo.com>
2023-02-12 22:43:19 +0330

    Display Categories in the navbar menu

---
+++ BlazorEcommerce/BlazorEcommerce/Client/Shared/NavMenu.razor.cs
@@ -0,0 +1,25 @@
+﻿using BlazorEcommerce.Client.Services.CategoryService;
+using Microsoft.AspNetCore.Components;
+using System.Runtime.CompilerServices;
+
+namespace BlazorEcommerce.Client.Shared
+{
+    public partial class NavMenu
+    {
+        private bool collapseNavMenu = true;
+        private string? NavMenuCssClass => collapseNavMenu ? "collapse" : null;
+
+        [Inject]
+        private ICategoryService categoryService { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            await categoryService.GetCategoriesAsync();
+        }
+
+        private void ToggleNavMenu()
+        {
+            collapseNavMenu = !collapseNavMenu;
+        }
+    }
+}
