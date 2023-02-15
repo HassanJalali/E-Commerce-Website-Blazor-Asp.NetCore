839d2cf92bcbb1a32a0e65e337b9910e11613a19
Hassan Jalali <mister.developer@yahoo.com>
2023-02-15 20:41:59 +0330

    syncfusion added to the client app

--- BlazorEcommerce/BlazorEcommerce/Client/Program.cs
+++ BlazorEcommerce/BlazorEcommerce/Client/Program.cs
@@ -3,11 +3,13 @@ using BlazorEcommerce.Client.Services.CategoryService;
 using BlazorEcommerce.Client.Services.ProductService;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
 
+Syncfusion.Licensing.SyncfusionLicenseProvider.RegisterLicense("ODY5OUAzMjMwMkUzMjJFMzBWblY5TGxVK1VXWHVRWmdTZ3g1OXdlUTlyNkxXOXJBTzFUQzVjZ21Sczk4PQ==");
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
-
+builder.Services.AddSyncfusionBlazor();
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
 builder.Services.AddScoped<IProductService, ProductService>();
 builder.Services.AddScoped<ICategoryService, CategoryService>();
