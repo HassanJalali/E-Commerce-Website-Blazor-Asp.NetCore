0fe9fb565a0636638eb7650d954f17069760fb9c
Hassan Jalali <mister.developer@yahoo.com>
2023-02-17 22:12:57 +0330

    revert

---
+++ BlazorEcommerce/BlazorEcommerce/Client/ServiceRegistration/ServiceRegistration.cs
@@ -0,0 +1,28 @@
+﻿using BlazorEcommerce.Client.Services.CategoryService;
+using BlazorEcommerce.Client.Services.HttpService;
+using BlazorEcommerce.Client.Services.ProductService;
+using CurrieTechnologies.Razor.SweetAlert2;
+using Framework.Front.Services;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Syncfusion.Blazor;
+
+namespace BlazorEcommerce.Client.ServiceRegistration
+{
+    public static class ServiceRegistration
+    {
+        public static void AddApplicationServices(this IServiceCollection services, WebAssemblyHostBuilder builder)
+        {
+            // using scrutor to find and register services
+            services.AddSyncfusionBlazor(options => { options.IgnoreScriptIsolation = true; });
+            services.AddSyncfusionBlazor();
+            services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+            services.AddScoped<IHttpService, HttpService>();
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddSweetAlert2(options =>
+            {
+                options.Theme = SweetAlertTheme.Dark;
+            });
+        }
+    }
+}
---
+++ BlazorEcommerce/BlazorEcommerce/Framework.Front/ApiServices/ApiServiceBase.cs
@@ -0,0 +1,42 @@
+﻿using Framework.Front.Model;
+using Framework.Front.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Framework.Front.ApiServices
+{
+    public abstract class ApiServiceBase
+    {
+        private readonly IHttpService _httpService;
+
+        public ApiServiceBase(IHttpService httpService)
+        {
+            _httpService = httpService;
+        }
+        public abstract string ApiUrl { get; set; }
+
+        public async Task<List<TModel>> GetAll<TModel>() where TModel : class, IModel
+        {
+            return await _httpService.Get<List<TModel>>(ApiUrl);
+        }
+        public async Task<TModel> FindById<TModel>(Guid id) where TModel : class, IModel
+        {
+            return await _httpService.Get<TModel>(ApiUrl + "/" + id);
+        }
+        public async Task Create<TModel>(TModel model) where TModel : class, IModel
+        {
+            await _httpService.Post<TModel>(ApiUrl, model);
+        }
+        public async Task Edit<TModel>(TModel model) where TModel : class, IModel
+        {
+            await _httpService.Put<TModel>(ApiUrl, model);
+        }
+        public async Task Delete(Guid id)
+        {
+            await _httpService.Delete(ApiUrl + "?Id" + id);
+        }
+    }
+}
